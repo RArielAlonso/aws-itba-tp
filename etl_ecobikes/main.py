ec0580e44853e87cd882089130dd569b11f8d318
RArielAlonso <rariel.alonso@gmail.com>
2022-12-23 14:36:18 -0300

    logging test

--- etl_ecobikes/main.py
+++ etl_ecobikes/main.py
@@ -1,11 +1,18 @@
 from extract import extract
+import logging
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                  level=logging.INFO,
+                  datefmt='%d-%b-%y %H:%M:%S')
 
 if __name__=='__main__':
+   logging.info('ETL Process has started')
    data_system_info,data_stations_info,data_stations_status=extract()
    print(data_system_info)
    print(data_stations_status)
    print(data_stations_info)
+   logging.info('ETL Process has ended succesfully')
    
 
    
