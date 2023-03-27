ec1dca6f250534a1e378336980935859ed5c862f
RArielAlonso <rariel.alonso@gmail.com>
2023-03-27 14:08:36 -0300

    backup lambda function

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -2,15 +2,16 @@ import json
 from geopy.distance import geodesic
 
 def lambda_handler(event, context):
+    
     if 'queryStringParameters' in event:
         distance=geodesic((event['queryStringParameters']['user_lat'],
-                           event['queryStringParameters']['user_lon']),
-                           (-34.598771,-58.374068)).m
-        body = 'The distance is {}'.format(distance)
+                            event['queryStringParameters']['user_lon']),
+                            (-34.598771,-58.374068)).m
+        body = 'La distancia a tu ecobici mas cercana es  {} metros'.format(round(distance))
         
     else:    # If no parameters
         print('No parameters!')
-        body = 'Please retry your parameters'
+        body = 'Who are you?'
     
     return {
         'statusCode': 200,
